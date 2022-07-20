ec17c4cdb55cec33e28311717d1ec80c5d2d8430
Wagner Galvao <wagnerjbg@primeit.com.br>
2022-07-20 15:25:48 -0300

    aula 3 - Configuracao do spring.jpa.defer-datasource-initialization em application.yml

--- src/main/java/com/example/demo/model/Pessoa.java
+++ src/main/java/com/example/demo/model/Pessoa.java
@@ -4,12 +4,12 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.TableGenerator;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
-@TableGenerator(name = "PESSOA")
+@Table
 @Data
 public class Pessoa {
 
